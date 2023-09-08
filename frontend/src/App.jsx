69ee95041ff83a681cf7e3be02ca8dafa16bc978
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-08 22:50:07 +0800

    Routes added for login, register, and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Login from './pages/Login';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 function App() {
-
   return (
     <>
-      <Login></Login>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/register" element={<Register/>}/>
+          </Routes>
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

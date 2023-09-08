c8d2575f68c78814a4e2bca39191390e5d414c2a
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-08 15:14:51 +0800

    Removed unused imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 

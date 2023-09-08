175a40f0d078818cfdd16284d8478f1a1d497611
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-08 15:33:16 +0800

    another unused import remove

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './components/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>

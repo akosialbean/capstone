c3477fd6d5f352d3d7b1e49c6a52e6406372e10e
Ritz Carl Feliciano <rc.feliciano013097@gmail.com>
2023-09-10 09:12:45 +0800

    react-bootstrap Reimport

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
